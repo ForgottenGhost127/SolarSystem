60946a6cf48ac645789300d570da1e254aa8a5de
LAPTOP-0CENV2U2\jufla <judith.legaz@hotmail.com>
2024-09-25 17:54:50 +0200

    Una prueba

--- Assets/Script/SolarSystem.cs
+++ Assets/Script/SolarSystem.cs
@@ -7,6 +7,7 @@ public class SolarSystem : MonoBehaviour
    public Transform SunTrans;
    public Transform Sat1Trans;
    public Transform Sat2Trans;
+   public Transform Sat3Trans;
    public Transform cameraM;
 
     void Start()
@@ -14,6 +15,7 @@ public class SolarSystem : MonoBehaviour
         SunTrans.position = new Vector3(10, 10, 10);
         Sat1Trans.position = new Vector3(SunTrans.position.x + 5, SunTrans.position.y + 5, SunTrans.position.z + 5);
         Sat2Trans.position = new Vector3(Sat1Trans.position.x + 2, Sat1Trans.position.y + 2, Sat1Trans.position.z + 2);
+        Sat3Trans.position = new Vector3(SunTrans.position.x + 9, SunTrans.position.y + 9, SunTrans.position.z + 9);
 
     }
 
@@ -31,16 +33,20 @@ public class SolarSystem : MonoBehaviour
         Sat2Trans.RotateAround(Sat1Trans.position, Vector3.right, 45f * Time.deltaTime);
         Sat2Trans.LookAt(Sat1Trans.position);
 
+        //Sat3
+        Sat3Trans.RotateAround(SunTrans.position, Vector3.up, -60f * Time.deltaTime);
+
         //Camera
         cameraM.LookAt(SunTrans.position);
 
         //DrawLines
         Debug.DrawLine(SunTrans.position, new Vector3(0, 0, 0), Color.blue);
         Debug.DrawLine(SunTrans.position, Sat1Trans.position, Color.red);
+        Debug.DrawLine(SunTrans.position, Sat3Trans.position, Color.cyan);
         Debug.DrawLine(SunTrans.position, Sat2Trans.position, Color.yellow);
         Debug.DrawLine(Sat1Trans.position, Sat2Trans.position, Color.green);
 
-        //Para intentar hacer que sat2 cambie su tamaño mientras gira debemos mantener la posicion, y cambiar la escala. 
+ 
         //Haz que Sat2 cambie de tamaño a la vez que gira. Si el ángulo entre su eje X y el eje X global es menor que 90 (usando Vector3.Angle), reducirá su tamaño. Si no, aumentará su tamaño.
         float angle = Vector3.Angle(Sat2Trans.right, Vector3.right);
 
@@ -50,7 +56,7 @@ public class SolarSystem : MonoBehaviour
         }
         else
         {
-            Sat2Trans.localScale = Vector3.Lerp(Sat2Trans.localScale, new Vector3(5f, 5f, 5f), Time.deltaTime);
+            Sat2Trans.localScale = Vector3.Lerp(Sat2Trans.localScale, new Vector3(3f, 3f, 3f), Time.deltaTime);
         }
     }
 }
